9d5a89aa75b4bea84a7d4152a133f7b3a79722a5
Antifishka <101981622+Antifishka@users.noreply.github.com>
2023-11-01 16:56:00 +0200

    init project

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+    ],
+  },
+  devServer: {
+    static: path.join(__dirname, 'dist'), // Роздільна директорія для devServer
+    port: 3000,
+    hot: true, // Вмикнути гаряче перезавантаження (Hot Module Replacement)
+  },
+};
